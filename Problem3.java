8f756f551bb4ab5cea1073e27ee8975800701391
Vlad Paraschiv <66594476+VladP03@users.noreply.github.com>
2020-10-20 18:20:46 +0300

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -4,7 +4,7 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
  */
 
-public class Main {
+public class Problem3 {
 
     public static long largestPrimeFactor(long number) {
 
