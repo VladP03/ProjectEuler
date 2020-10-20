f1c4717b0aea2ae8b1904447c075849e87d12714
Vlad Paraschiv <66594476+VladP03@users.noreply.github.com>
2020-10-20 18:32:23 +0300

    Update and rename Main.java to Problem8.java

--- Problem8.java
+++ Problem8.java
@@ -25,7 +25,7 @@ The four adjacent digits in the 1000-digit number that have the greatest product
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 */
 
-public class Main {
+public class Problem8 {
 
     public static long[] vector(long []a) {
 
