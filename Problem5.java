ae86fc5273cab0cbcfc55a2d442b9378a8ca13aa
Vlad Paraschiv <66594476+VladP03@users.noreply.github.com>
2020-10-20 18:21:15 +0300

    Update and rename 5.SmallestMultiple.java to Problem5.java

--- Problem5.java
+++ Problem5.java
@@ -4,7 +4,7 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  */
 
-public class Main {
+public class Problem5 {
 
     public static boolean isDivededBy1to20(int number) {
 
