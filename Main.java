43358d3fe32c552121dc3bf3e2434bdae322c80a
Vlad Paraschiv <66594476+VladP03@users.noreply.github.com>
2020-10-20 18:28:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+public class Main {
+
+    public static boolean isPrime(int number) {
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int prime = 2;
+        for (int i = 5; i < Integer.MAX_VALUE; i++) {
+            if (isPrime(i)) {
+                prime++;
+            }
+            if (prime == 10_001) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
