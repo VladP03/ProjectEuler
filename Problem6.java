5cc323b1bc6e469af71d0c056059e3a788c03657
Vlad Paraschiv <66594476+VladP03@users.noreply.github.com>
2020-10-20 18:24:53 +0300

    Update and rename Main.java to Problem6.java

--- Problem6.java
+++ Problem6.java
@@ -8,7 +8,7 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  */
 
-public class Main {
+public class Problem6 {
 
     public static int sumOfSquare(int number) {
 
